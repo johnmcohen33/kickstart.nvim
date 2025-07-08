2fe4932041652672446a6a7efc9bdb379659c217
johnmcohen33 <johnmcohen33@gmail.com>
2025-07-08 12:26:09 -0400

    add ftplugins

--- init.lua
+++ init.lua
@@ -768,6 +768,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        html = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
